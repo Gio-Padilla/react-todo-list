94910b125717787a14dbe5a83dcba52070b6d668
Gio-Padilla <pad24005@byui.edu>
2026-05-12 16:43:13 -0500

    Added the ability to mark the added Todos

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,20 @@ export default function App() {
         { id: crypto.randomUUID(), title: newItem, complete: false},
       ]
     })
+
+    setNewItem("")
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+
+        return todo
+      })
+    })
   }
 
   return (
@@ -28,9 +42,9 @@ export default function App() {
       <h1 className="header">Todo List</h1>
       <ul className="list">
         {todos.map(todo => {
-          return <li>
+          return <li key={todo.id}>
             <label>
-              <input type="checkbox" checked={todo.completed}/>
+              <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)}/>
               {todo.title}
             </label>
             <button className="btn btn-danger">Delete</button>
